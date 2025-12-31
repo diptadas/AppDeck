9bbbaf95c331f53897471372e5f28934af79a870
Dipta Das <dipta670@gmail.com>
2025-12-31 00:00:00 -0700

    Select light or dark theme based on system setting

--- app/src/main/java/com/appdeck/app/Theme.kt
+++ app/src/main/java/com/appdeck/app/Theme.kt
@@ -1,9 +1,21 @@
 package com.appdeck.app
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AppDeckTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
+    val colorScheme = if (isSystemInDarkTheme()) {
+        darkColorScheme()
+    } else {
+        lightColorScheme()
+    }
+    
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
 }
